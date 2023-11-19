15503867f46f7fc7259d6809877840d6cd044968
Miguel Paricahua Lopez <mig.parlop@gmail.com>
2023-11-18 20:58:08 -0500

    responsive for pages

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -46,19 +46,19 @@ function Dashboard() {
       <div className="w-full">
         <Header title="Dashboard" />
       </div>
-      <div className="grow flex items-center justify-around">
-        <div className="h-[50%] bg-white w-[40%] rounded-xl flex flex-col justify-around">
-          <h1 className="text-center text-[3rem] text-black/60 font-bold">
+      <div className="grow flex items-center justify-around flex-col md:flex-row">
+        <div className="h-[50%] bg-white w-[80%] md:w-[40%] rounded-xl flex flex-col justify-around">
+          <h1 className="text-center text-[2rem] md:text-[2.4rem] lg:text-[3rem] text-black/60 font-bold">
             Bienvenido
           </h1>
           {isUserActive && !isLoading && !isError && (
-            <p className="text-[2rem] text-center">{`${user.nombre} ${user.apellido}`}</p>
+            <p className="text-[1.4rem] md:text-[2rem] text-center">{`${user.nombre} ${user.apellido}`}</p>
           )}
         </div>
-        <div className="h-[50%] bg-white w-[40%] rounded-xl flex flex-col items-center justify-around">
+        <div className="my-[2rem] md:my-0 h-[50%] bg-white w-[80%] md:w-[40%] rounded-xl flex flex-col items-center justify-around">
           <div className="h-[30%] w-full flex bg-primary/95 items-center px-[1rem] rounded-t-xl justify-center">
             <img className="h-[60%] text-white" src={UserIcon} alt="" />
-            <p className="text-[1.4rem] pl-6 text-white font-bold ">
+            <p className=" text-[1rem] md:text-[1.4rem] pl-6 text-white font-bold ">
               Usuarios activos
             </p>
           </div>
